55edb43f22872d299e9b748fcfb850b19b3ffc53
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2021-10-05 17:53:31 +0500

    Release Nixware Locations

---
+++ Nixware/Atlon Helper/TD_Location.lua
@@ -0,0 +1,35 @@
+location = {
+-- Molotovs
+{ 'de_mirage', 'molotov', 'CT Stairs', vec3_t.new(-131.57, -1418.04, -72.18), vec3_t.new(-3.94, -58.84, 0), 'Throw+Run+Jump', 5, 1, 26 },
+{ 'de_mirage', 'molotov', 'Upper Palace', vec3_t.new(143.13, -1583.88, -172.78), vec3_t.new(0.0, -125.26, 0), 'Throw+Run', 2, 1, 19 },
+{ 'de_mirage', 'molotov', 'Under Palace', vec3_t.new(105.59, -1636.73, -167.97), vec3_t.new(2.7, -125.26, 0), 'Throw', 1, 0 },
+{ 'de_mirage', 'molotov', 'Under Palace [ 2 ]', vec3_t.new(-736.89, -1306.05, -167.97), vec3_t.new(-13.57, -53.29, 0), 'Throw+Run', 1, 0, 18 },
+{ 'de_mirage', 'molotov', 'Under Palace [ 3 ]', vec3_t.new(322.24, -2294.25, -39.97), vec3_t.new(-8.46, -163.64, 0), 'Throw+Run', 10, 0, 32 },
+{ 'de_mirage', 'molotov', 'Ramp', vec3_t.new(150.96, -1914.06, -39.97), vec3_t.new(16.40, -154.60, 0), 'Throw+Run', 1, 0, 12 },
+{ 'de_mirage', 'molotov', 'Plant', vec3_t.new(150.55, -2375.43, -39.97), vec3_t.new(10.95, 121.60, 0), 'Throw', 1, 0 },
+{ 'de_mirage', 'molotov', 'Catwalk', vec3_t.new(-495.87, -1618.05, -39.97), vec3_t.new(-18.3, 103.19, 0), 'Throw+Run', 8, 0, 22 },
+{ 'de_mirage', 'molotov', 'Box', vec3_t.new(-879.97, -1306.03, -167.97), vec3_t.new(-29.87, -88.09, 0), 'Throw+Run', 1, 0, 6 },
+{ 'de_mirage', 'molotov', 'Trash', vec3_t.new(-783.95, -2088.04, -179.97), vec3_t.new(-19.30, -85.53, 0), 'Throw+Run', 2, 0, 13 },
+{ 'de_mirage', 'molotov', 'Short', vec3_t.new(-1038.93, -251.06, -367.97), vec3_t.new(-6.87, -73.0, 0), 'Throw+Run', 2, 0, 12 },
+{ 'de_mirage', 'molotov', 'Under T Stairs', vec3_t.new(-543.34, -1389.01, -117.56), vec3_t.new(4.74, -66.68, 0), 'Throw+Run', 10, 0, 26 },
+{ 'de_mirage', 'molotov', 'Ladder', vec3_t.new(-966.73, -233.74, -363.97), vec3_t.new(-12.81, -105.54, 0), 'Throw+Run+Jump', 0, 0, 36 },
+{ 'de_mirage', 'molotov', 'Chair', vec3_t.new(-719.98, -962.41, -215.97), vec3_t.new(-0.99, 70.43, 0), 'Throw+Run+Jump', 2, 0, 36 },
+{ 'de_mirage', 'molotov', 'Connector', vec3_t.new(-473.17, -544.59, -270.38), vec3_t.new(-15.17, -119.43, 0), 'Throw+Run', 1, 0, 40 },
+{ 'de_mirage', 'molotov', 'Connector [ Right ]', vec3_t.new(-851.44, -619.34, -261.95), vec3_t.new(-18.09, -51.21, 0), 'Throw+Run', 10, 0, 68 },
+{ 'de_mirage', 'molotov', 'Plant [ Left ]', vec3_t.new(-2050.29, -532.38, -167.97), vec3_t.new(-16.92, 58.83, 0), 'Throw+Run', 2, 0, 26 },
+-- He grenades
+{ 'de_mirage', 'hegrenade', 'Upper Palace', vec3_t.new(143.13, -1583.88, -172.78), vec3_t.new(0.0, -125.26, 0), 'Throw+Run', 2, 1, 23 },
+{ 'de_mirage', 'hegrenade', 'Under Palace', vec3_t.new(115.84, -1616.66, -169.50), vec3_t.new(5.67, -124.98, 0), 'Throw+Run', 1, 0, 12 },
+{ 'de_mirage', 'hegrenade', 'Under Palace [ 2 ]', vec3_t.new(322.24, -2294.25, -39.97), vec3_t.new(-8.46, -163.64, 0), 'Throw+Run', 10, 0, 32 },
+{ 'de_mirage', 'hegrenade', 'Plant', vec3_t.new(150.55, -2375.43, -39.97), vec3_t.new(10.95, 121.60, 0), 'Throw', 1, 0 },
+{ 'de_mirage', 'hegrenade', 'Name', vec3_t.new(49.69, -1579.16, -173.27), vec3_t.new(53.79, -0.02, 0), 'Throw+Run+Jump', 6, 0, 52 },
+{ 'de_mirage', 'hegrenade', 'Short', vec3_t.new(-1038.93, -251.06, -367.97), vec3_t.new(-6.87, -73.0, 0), 'Throw+Run', 2, 0, 20 },
+{ 'de_mirage', 'hegrenade', 'Ladder', vec3_t.new(-966.73, -233.74, -363.97), vec3_t.new(-12.81, -105.54, 0), 'Throw+Run+Jump', 0, 0, 36 },
+{ 'de_mirage', 'hegrenade', 'Chair', vec3_t.new(-719.98, -962.41, -215.97), vec3_t.new(-5.99, 70.43, 0), 'Throw+Run+Jump', 10, 0, 52 },
+{ 'de_mirage', 'hegrenade', 'Connector', vec3_t.new(-473.17, -544.59, -270.38), vec3_t.new(-15.17, -119.43, 0), 'Throw+Run', 1, 0, 40 },
+{ 'de_mirage', 'hegrenade', 'Connector [ Right ]', vec3_t.new(-851.44, -619.34, -261.95), vec3_t.new(-15.09, -51.21, 0), 'Throw+Run', 10, 0, 68 },
+{ 'de_mirage', 'hegrenade', 'Holding', vec3_t.new(-1426.56, 808.00, -48.93), vec3_t.new(4.62, -141.13, 0), 'Throw+Run', 2, 0, 19 },
+{ 'de_mirage', 'hegrenade', 'Plant [ Left ]', vec3_t.new(-2050.29, -532.38, -167.97), vec3_t.new(-14.92, 58.83, 0), 'Throw+Run', 2, 0, 26 },
+}
+
+return { e_default = true, e_custom = true, load_l = location }
