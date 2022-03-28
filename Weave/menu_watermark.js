e968d23be83d77f4880ece0c4739fa081768d8ed
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2022-03-28 21:01:58 +0500

    Fixed number 2

--- Weave/menu_watermark.js
+++ Weave/menu_watermark.js
@@ -1,7 +1,9 @@
 var anim = 0
 //Made by Mased
 register_callback("render", function() { 
-    if (ui.get_menu_alpha() > 0) {
+    var a = ui.get_menu_alpha()
+
+    if (a > 0) {
         lerp = function(start, end, time, do_extraanim) { if(!do_extraanim && Math.floor(start) == end) return end; time = global_vars.frametime() * (time * 175); if(time < 0) time = 0.01; else if(time > 1) time = 1; return (end - start) * time + start }
         anim = lerp(anim, ui.get_menu_alpha() ? 255 : 0, 0.03)
         var pos = ui.get_menu_position()
@@ -9,14 +11,12 @@ register_callback("render", function() {
         var hours = new Date().getHours()
         var minutes = new Date().getMinutes()
 
-        render.filled_rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [16,16,16, 255* (anim / 255)], 4)
-        render.rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [23, 98, 219, 255 * (anim / 255)], 4)
-        render.text([pos[0] + 10 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * (anim / 255)], 12, 3, "Welcome back, Mased")
-        render.text([pos[0] + size[0] - 60 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * (anim / 255)], 12, 3, hours + ":" + minutes)
+        render.filled_rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [16,16,16, 255 * a], 4)
+        render.rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [23, 98, 219, 255 * a], 4)
+        render.text([pos[0] + 10 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * a], 12, 3, "Welcome back, Mased")
+        render.text([pos[0] + size[0] - 60 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * a], 12, 3, hours + ":" + minutes)
 
-        render.line([pos[0], pos[1]], [pos[0] + 20 * (anim / 255), pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * (anim / 255)], 1)
-        render.line([pos[0] + size[0], pos[1]], [pos[0] - 1 + 20 * (anim / 255) + size[0], pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * (anim / 255)], 1)
-
+        render.line([pos[0], pos[1]], [pos[0] + 20 * (anim / 255), pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * a], 1)
+        render.line([pos[0] + size[0], pos[1]], [pos[0] - 1 + 20 * (anim / 255) + size[0], pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * a], 1)
     } else { return }
-
 })
