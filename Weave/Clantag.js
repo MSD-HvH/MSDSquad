d175d42939c55d4cfa3bea0acae3d18283179099
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2022-03-31 11:56:31 +0500

    Create Clantag.js

---
+++ Weave/Clantag.js
@@ -0,0 +1,19 @@
+ui.add_slider("Speed", "speed", 1, 10)
+
+var text = [
+    "M", "MS", "MSD", "MSDS", "MSDSq", "MSDSqu", "MSDSqua", "MSDSquad",
+    "MSDSquad", "MSDSqua", "MSDSqu", "MSDSq", "MSDS", "MSD", "MS", "M" 
+];
+var lastTime = 0;
+
+register_callback("render", function() {
+    var time = parseInt((global_vars.curtime()) * vars.get_uint("js.speed"))
+
+    if(time != lastTime) {
+        for(i in text) {
+            if((time % text.length) == i) {
+                cheat.set_clantag(text[i])
+            }
+        }
+    }
+})
