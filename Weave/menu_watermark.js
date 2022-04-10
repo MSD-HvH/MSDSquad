098dab984bf89f0e9a29264b033c583df994df65
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2022-04-10 15:17:06 +0500

    better animation

--- Weave/menu_watermark.js
+++ Weave/menu_watermark.js
@@ -1,22 +1,27 @@
-var anim = 0
-//Made by Mased
+var anim = 0;
+var x = 0;
+var y = 0;
+var today = new Date();
+var hours = today.getHours() <= 9 ? "0" + today.getHours() + ":" : today.getHours();
+var minutes = today.getMinutes() <= 9 ? "0" + today.getMinutes() : today.getMinutes();
+var lerp = function(a, b, percentage) { return a + (b - a) * percentage; }
+
 register_callback("render", function() { 
-    var a = ui.get_menu_alpha()
+    var a = ui.get_menu_alpha() //Made by Mased
+    var pos = ui.get_menu_position()
+    var size = ui.get_menu_size()
+
+    anim = lerp(anim, ui.get_menu_alpha() ? 255 : 0, 0.5)
+    x = lerp(x, pos[0] + 100, global_vars.frametime() * 3); 
+    y = lerp(y, pos[1] + 150, global_vars.frametime() * 3)
 
     if (a > 0) {
-        lerp = function(start, end, time, do_extraanim) { if(!do_extraanim && Math.floor(start) == end) return end; time = global_vars.frametime() * (time * 175); if(time < 0) time = 0.01; else if(time > 1) time = 1; return (end - start) * time + start }
-        anim = lerp(anim, ui.get_menu_alpha() ? 255 : 0, 0.03)
-        var pos = ui.get_menu_position()
-        var size = ui.get_menu_size()
-        var hours = new Date().getHours()
-        var minutes = new Date().getMinutes()
+        render.filled_rect([x - 80, y - 220], [size[0], 40], [16,16,16, 255 * a], 4)
+        render.rect([x - 80, y - 220], [size[0], 40], [23, 98, 219, 255 * a], 4)
+        render.text([x - 60, y - 200], [255, 255, 255, 255 * a], 12, 3, "Welcome back, Mased")
+        render.text([x + size[0] - 140, y - 200], [255, 255, 255, 255 * a], 12, 3, hours + ":" + minutes)
 
-        render.filled_rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [16,16,16, 255 * a], 4)
-        render.rect([pos[0] + 20 * (anim / 255), pos[1] - 70 * (anim / 255)], [size[0], 40], [23, 98, 219, 255 * a], 4)
-        render.text([pos[0] + 10 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * a], 12, 3, "Welcome back, Mased")
-        render.text([pos[0] + size[0] - 60 + 20 * (anim / 255), pos[1] + 20 - 70 * (anim / 255)], [255, 255, 255, 255 * a], 12, 3, hours + ":" + minutes)
-
-        render.line([pos[0], pos[1]], [pos[0] + 20 * (anim / 255), pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * a], 1)
-        render.line([pos[0] + size[0], pos[1]], [pos[0] - 1 + 20 * (anim / 255) + size[0], pos[1] + 38 - (70 * (anim / 255))], [255, 255, 255, 255 * a], 1)
-    } else { return }
+        render.line([x - 80, y - 185], [pos[0], pos[1]], [255, 255, 255, 100 * a], 0.5)
+        render.line([x + size[0] - 80, y - 185], [pos[0] + size[0], pos[1]], [255, 255, 255, 100 * a], 0.5)
+    }
 })
