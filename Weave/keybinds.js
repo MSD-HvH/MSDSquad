a42b0f74a05bd91a05a24589a13a27320105b85d
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2022-05-05 23:13:56 +0500

    added animation

--- Weave/keybinds.js
+++ Weave/keybinds.js
@@ -1,6 +1,7 @@
 var screen = render.get_screen_size(); 
-var anim = 0; //Made by Mased
-lerp = function(start, end, time, do_extraanim) { if(!do_extraanim && Math.floor(start) == end) return end; time = global_vars.frametime() * (time * 175); if(time < 0) time = 0.01; else if(time > 1) time = 1; return (end - start) * time + start }
+var lerp = function(start, end, time, do_extraanim) { if(!do_extraanim && Math.floor(start) == end) return end; time = global_vars.frametime() * (time * 175); if(time < 0) time = 0.01; else if(time > 1) time = 1; return (end - start) * time + start }
+var menu_anim = 0
+var data = new Object
 var binds = [
     ["doubletap", "double piu-piu"],
     ["hide_shots", "hide shots"],
@@ -15,22 +16,33 @@ ui.add_slider("Width", "width", 150, 300)
 ui.add_slider("Position x", "x", 5, screen[0]);
 ui.add_slider("Position y", "y", 5, screen[1])
 
+for(i in binds) {
+    data[binds[i][0]] = {
+        name: binds[i][1],
+        anim: 0,
+        offset: 0
+    }
+}
+
 register_callback("render", function() {
     var x = vars.get_uint("js.x"), y = vars.get_uint("js.y"), width = vars.get_uint("js.width")
-    var activeBinds = []
+    var masterActive = []
 
-    render.line([x, y - 1], [x + width, y - 1], [170, 100, 255, 255 * (anim / 255)], 2)
-    render.filled_rect([x, y], [width, 15], [15, 15, 15, 255 * (anim / 255)], 3)
-    render.text([x + 5, y], [255, 255, 255, 255 * (anim / 255)], 9, 1, "keybinds")
-    
-    for(i in binds) {
-        if(vars.is_bind_active(binds[i][0])) { activeBinds.push(binds[i][1]); }
+    render.line([x, y - 1], [x + width, y - 1], [170, 100, 255, 255 * (menu_anim / 255)], 2)
+    render.filled_rect([x, y], [width, 15], [15, 15, 15, 255 * (menu_anim / 255)], 3)
+    render.text([x + 5, y], [255, 255, 255, 255 * (menu_anim / 255)], 9, 1, "keybinds")
+
+    for(i in data) {
+        data[i].anim = lerp(data[i].anim, (vars.is_bind_active(i) || ui.get_menu_alpha()) ? 255 : 0, 0.04)
+        if(vars.is_bind_active(i)) {
+            masterActive.push(data[i])
+        }
     }
 
-    for(var i = 0; i < activeBinds.length; i++) {
-        render.text([x + 5, y + 17 * (anim / 255) + (12 * i)], [255, 255, 255, 255 * (anim / 255)], 1, 1, activeBinds[i])
-        render.text([x + width - 50, y + 17 * (anim / 255) + (12 * i)], [255, 255, 255, 255 * (anim / 255)], 1, 1, "[ toggled ]" )
+    for(i = 0; i < masterActive.length; i++) {
+        render.text([x + 5, y + 17 * (masterActive[i].anim / 255) + (13 * i)], [255, 255, 255, 255 * (masterActive[i].anim / 255)], 1, 1, masterActive[i].name)
+        render.text([x + width - 50, y + 17 * (masterActive[i].anim / 255) + (13 * i)], [255, 255, 255, 255 * (masterActive[i].anim / 255)], 1, 1, "[ toggled ]" )
     }
 
-    anim = lerp(anim, (activeBinds.length != 0 || ui.get_menu_alpha()) ? 255 : 0, 0.03)
+    menu_anim = lerp(menu_anim, (masterActive.length != 0 || ui.get_menu_alpha()) ? 255 : 0, 0.03)
 })
