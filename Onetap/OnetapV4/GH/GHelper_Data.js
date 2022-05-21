635a31d62da488d42be72e792591f8be36ad250f
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2022-05-21 12:20:33 +0500

    Create GHelper_Data.js

---
+++ Onetap/OnetapV4/GH/GHelper_Data.js
@@ -0,0 +1,614 @@
+var locations = {
+  "de_mirage": [
+    //Molotovs de_mirage
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "(Unsafe for pit)", 
+      "position" : [875.96875, -1218.4539794921875, -108.96875], 
+      "viewangles" : [-21, -164],
+      "grenade" : {
+            "run": 1,
+            "run_speed": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Pit Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [875.96875, -1217.477783203125, -108.96875], 
+      "viewangles" : [1.9810665845870972, -164.08090209960938],
+      "grenade" : {
+            "run": 12,
+            "jump": true,
+            "delay": 5
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck near Palace"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [875.9107666015625, -1194.20556640625, -108.96875], 
+      "viewangles" : [-27.22723388671875, -146.62258911132813],
+      "grenade" : {
+            "jump": true,
+            "delay": 21
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "A Site Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [678.3862915039063, -1176.3695068359375, -127.96875], 
+      "viewangles" : [-37, -138.12295532226563],
+      "grenade" : {
+            "run": 13,
+            "run_speed": true,
+            "jump": true,
+            "delay": 23
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "", 
+      "position" : [-51.48946762084961, -1418.03125, -120.17536926269531], 
+      "viewangles" : [-14, -69.91244506835938],
+      "grenade" : {
+            "run": 6,
+            "jump": true,
+            "delay": 6
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "unsafe", 
+      "position" : [-113.57327270507813, -1470.3116455078125, -69.17536926269531], 
+      "viewangles" : [14, 168.1363983154297],
+      "grenade" : {
+            "run": 14,
+            "run_yaw": 90
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-24.307357788085938, -1556.396484375, -167.96875], 
+      "viewangles" : [-30.363012313842773, 168.83543395996094]
+    },
+    
+    { 
+      "name" : ["UNNAMED", "Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [308.0524597167969, -1651.6763916015625, -175.96875], 
+      "viewangles" : [9, 122],
+      "grenade" : {
+            "run": 13,
+            "jump": true,
+            "delay": 1
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Under Tetris"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-530.891357421875, -1421.560546875, -91.5115737915039], 
+      "viewangles" : [9.058109283447266, -66.50411224365234],
+      "grenade" : {
+            "run": 9
+      }
+    },  
+
+    { 
+      "name" : ["UNNAMED", "Plant"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-554.0401000976563, -1309.03125, -163.96875], 
+      "viewangles" : [-14.512494087219238, -74.1092529296875]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Palace Enter"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-624.9771728515625, -1354.1849365234375, -167.96875], 
+      "viewangles" : [-32.21943283081055, -50.30895233154297]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Under Palace"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-624.9771118164063, -1354.2164306640625, -167.96875], 
+      "viewangles" : [-15.558889389038086, -54.223880767822266]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Connector Box Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-322.8639831542969, -1769.9033203125, -167.96875], 
+      "viewangles" : [-25.48636245727539, 125.39749908447266],
+      "grenade" : {
+            "strength": 0.5
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Palace Far"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-624.998046875, -1354.1893310546875, -167.96875], 
+      "viewangles" : [-22.177141189575195, -51.95741653442383],
+      "grenade" : {
+            "run": 4
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Palace Near"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-624.998046875, -1354.1893310546875, -167.96875], 
+      "viewangles" : [-18.5196590423584, -52.02050018310547],
+      "grenade" : {
+            "run": 5,
+            "run_speed": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "T Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-601.6357421875, -1544.73876953125, -167.96875], 
+      "viewangles" : [-47.662715911865234, 6.030156135559082]
+    },
+
+    { 
+      "name" : ["UNNAMED", "T Stairs + UnderStais"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-601.6583862304688, -1544.7215576171875, -167.96875], 
+      "viewangles" : [-41.857181549072266, 10.32628345489502]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Under Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-601.676513671875, -1544.7457275390625, -167.96875], 
+      "viewangles" : [-46.32748794555664, 0.6890011429786682],
+      "grenade" : {
+            "strength": 0.5
+      }
+    },    
+
+    { 
+      "name" : ["UNNAMED", "Sandwich"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-601.6397094726563, -1544.76318359375, -167.96875], 
+      "viewangles" : [-61.30576705932617, 8.120174407958984]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Jungle + Column"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [181.96875, -1717.96875, -167.96875], 
+      "viewangles" : [-15.675853729248047, 166.79367065429688],
+      "grenade" : {
+            "run": 8,
+            "run_speed": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Chair"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-658.4036254882813, -920.1634521484375, -223.8916778564453], 
+      "viewangles" : [-3.6567156314849854, 72.43115997314453],
+      "grenade" : {
+            "run": 18,
+            "jump": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-429.81903076171875, -1637.662109375, -39.96875], 
+      "viewangles" : [4.99363374710083, 100.39697265625],
+      "grenade" : {
+            "run": 25,
+            "run_yaw": 24,
+            "jump": true,
+            "delay": 2
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "VentRoom"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-429.76031494140625, -1637.718505859375, -39.96875], 
+      "viewangles" : [1.1, 95.45394744873047],
+      "grenade" : {
+            "run": 17,
+            "run_yaw": 34,
+            "jump": true,
+            "delay": 9
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Ticket Booth"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [-669.6957397460938, -773.5470581054688, -261.96875], 
+      "viewangles" : [0.2910824716091156, -94.57435607910156],
+      "grenade" : {
+            "run": 28,
+            "jump": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "VentRoom"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [-964.64208984375, -204.39654541015625, -363.96875], 
+      "viewangles" : [-16.428932189941406, -104.08546447753906],
+      "grenade" : {
+            "run": 32,
+            "jump": true
+      }
+    }, 
+
+    { 
+      "name" : ["UNNAMED", "Short"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1044.0247802734375, -183.19329833984375, -367.96875], 
+      "viewangles" : [-10.275069236755371, -78.17743682861328],
+      "grenade" : {
+            "run": 13
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1047.96875, 117.20692443847656, -359.96875], 
+      "viewangles" : [-58.286956787109375, 121.12984466552734]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [-975.58056640625, 268.29541015625, -367.96875], 
+      "viewangles" : [-33.20690155029297, -20],
+      "grenade" : {
+            "run": 25,
+            "run_yaw": 90
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Plant"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Damage from fall", 
+      "position" : [-668.5762939453125, -516.7354736328125, -48.09375], 
+      "viewangles" : [5.574137210845947, -79.85685729980469],
+      "grenade" : {
+            "run": 14,
+            "run_speed": true,
+            "jump": true,
+            "delay": 2
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-916.4181518554688, -518.7805786132813, -48.09375], 
+      "viewangles" : [-19.099485397338867, -162.5924530029297]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-921.1218872070313, -449.68841552734375, -48.09375], 
+      "viewangles" : [-16.951440811157227, -151.88369750976563]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck Shop Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Damage from fall", 
+      "position" : [-920.0433349609375, -380.0312194824219, -48.09375], 
+      "viewangles" : [-2.843775749206543, -121.97026824951172],
+      "grenade" : {
+            "run": 10,
+            "jump": true,
+            "delay": 7
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "T Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [-538.3914184570313, -1492.06494140625, -39.96875], 
+      "viewangles" : [-15.61619758605957, -22.40366554260254],
+      "grenade" : {
+            "run": 4
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [150.96896362304688, -1914.03125, -39.96875], 
+      "viewangles" : [-11.436200141906738, -125.62914276123047],
+      "grenade" : {
+            "run": 19,
+            "jump": true,
+            "delay": 2
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck Box"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [16.031261444091797, -2234.422119140625, -39.96875], 
+      "viewangles" : [-7.140079975128174, 16.14017105102539],
+      "grenade" : {
+            "run": 3
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Column Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [972.7157592773438, -1871.677978515625, -71.96875], 
+      "viewangles" : [-0.3475989103317261, -134.39280700683594],
+      "grenade" : {
+            "run": 1
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Under Palace"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [319.3183288574219, -2300.357421875, -39.96875], 
+      "viewangles" : [-7.082022190093994, -164.40191650390625],
+      "grenade" : {
+            "run": 19
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Pit Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-44.90926742553711, -1921.90380859375, -39.96875], 
+      "viewangles" : [3.1938698291778564, 69.27425384521484],
+      "grenade" : {
+            "run": 20,
+            "jump": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-752.03125, -40.622928619384766, -161.1770782470703], 
+      "viewangles" : [-20.666961669921875, 145.2179718017578]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Chair"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1035.837890625, 295.9687805175781, -159.96875], 
+      "viewangles" : [-14.164755821228027, -57.86521530151367],
+      "grenade" : {
+            "run": 3
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short Controller"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1035.809814453125, 295.96881103515625, -159.96875], 
+      "viewangles" : [-11.320034980773926, -57.57488250732422],
+      "grenade" : {
+            "run": 3
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Tunnel Enter"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1035.7767333984375, 295.96881103515625, -159.96875], 
+      "viewangles" : [-21.770095825195313, -55.02043914794922],
+      "grenade" : {
+            "run": 7
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Middle Window"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1035.796142578125, 295.9688720703125, -159.96875], 
+      "viewangles" : [-2.1473331451416016, -54.6695442199707],
+      "grenade" : {
+            "run": 31,
+            "jump": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Bench Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-940.5447998046875, 294.97625732421875, -167.18307495117188], 
+      "viewangles" : [-11.087865829467773, -85.37838745117188]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Plant"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1325.332763671875, 300.0223388671875, -167.96875], 
+      "viewangles" : [-15.035666465759277, 146.4929656982422],
+      "grenade" : {
+            "run": 9,
+            "run_speed": true
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck Van"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1325.36376953125, 300.0255432128906, -167.96875], 
+      "viewangles" : [-17.415939331054688, 150.84957885742188]
+    },
+
+    { 
+      "name" : ["UNNAMED", "B Plant Corner"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-2134.939697265625, -594.95751953125, -167.96875], 
+      "viewangles" : [-10.391217231750488, 53.318809509277344],
+      "grenade" : {
+            "run": 15
+      }
+    },
+    
+    { 
+      "name" : ["UNNAMED", "B Plant"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-1690.9229736328125, -660.8856811523438, -167.96875], 
+      "viewangles" : [-9.055948257446289, 122.81363677978516],
+      "grenade" : {
+            "run": 4
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Middle Window"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [291.22662353515625, -169.17929077148438, -166.11831665039063], 
+      "viewangles" : [-16.835418701171875, -150.78973388671875],
+      "grenade" : {
+            "run": 35
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [291.252197265625, -169.1602020263672, -166.11302185058594], 
+      "viewangles" : [0.5813681483268738, -137.8383026123047],
+      "grenade" : {
+            "run": 34,
+            "jump": true,
+            "delay": 1
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "CT Stairs"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [130.0046844482422, -887.1443481445313, -39.96875], 
+      "viewangles" : [4.122797012329102, -158.96534729003906],
+      "grenade" : {
+            "run": 12,
+            "jump": true,
+            "delay": 4
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Secret"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Unsafe", 
+      "position" : [-1345.5235595703125, -2439.22216796875, -206.9969024658203], 
+      "viewangles" : [3.308321952819824, 72.85321044921875],
+      "grenade" : {
+            "run": 20,
+            "run_yaw": 97
+      }
+    },
+
+    { 
+      "name" : ["UNNAMED", "Short"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-699.5624389648438, 558.8740234375, -83.26274108886719], 
+      "viewangles" : [-59.97234344482422, -97.62185668945313]
+    },
+
+    { 
+      "name" : ["UNNAMED", "Fakeduck"], 
+      "weapon" : "weapon_molotov", 
+      "description" : "Safe", 
+      "position" : [-22.041486740112305, 817.9490966796875, -135.96875], 
+      "viewangles" : [-11.960465431213379, -154.80372619628906],
+      "grenade" : {
+            "run": 6
+      }
+    },
+    //HE Grenade de_mirage
+    { 
+      "name" : ["UNNAMED", "Connector Box Fakeduck"], 
+      "weapon" : "weapon_hegrenade", 
+      "description" : "Safe", 
+      "position" : [-322.84814453125, -1769.876220703125, -167.96875], 
+      "viewangles" : [-30, 125.45828247070313],
+      "grenade" : {
+            "strength": 0.5,
+            "run": 3,
+            "run_speed": true
+      }
+    },
+  ],
+
+  "de_dust2": [
+
+  ]
+}
+
+exports.locations = locations;
