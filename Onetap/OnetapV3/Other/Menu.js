a60a89b6ddfda06f319f33accbf14622b78c013a
Mased <68379695+MasedMSD@users.noreply.github.com>
2022-10-19 19:31:58 +0500

    some remake, functions and going to recode

--- Onetap/OnetapV3/Other/Menu.js
+++ Onetap/OnetapV3/Other/Menu.js
@@ -1,96 +1,95 @@
-const Useful = require("./useful.js")
+const Useful = require("./useful.js");
 const screen = Render.GetScreenSize();
-const build = "release";
+const build = Cheat.GetUsername() === "Mased" ? "dev" : "release";
 
 /**
-    И так начнёмс. Дата у нас 13.10.22 | 22:08
-    Время наебашить меню для в3.
-
-    Author: Mased
-    Discord: Mased#1854
-    Telegram: MasedMSD
-
-    https://brokencore.club/members/1529
-    https://yougame.biz/members/228508
-
-    Текущая версия меню: 3.0.0
-
-    Начнём с пару слов. Кфг системы не будет, джска сделана по фану.
-    Если возникли вопрос то пишите в дискорд.
-*/
-
-/**
- * @param { string } name Название меню
- * @param { function= } cb Callback первым аргументом которого является this
+ * @param {string} method Метод в котором появляется ошибка
+ * @param {string} text Описание ошибки
  */
-exports.CreateMenu = function(name, cb) {
-    const error = function(method, text) {
-        throw new Error(Useful.Other.FormatString("[Menu] {{method}}: {{text}}", { method, text }));
-    };
+const throw_error = function(method, text) {
+    throw new Error(Useful.Other.FormatString("[Menu] {{method}}: {{text}}", { method, text }));
+};
 
-    /** @private */
-    const validation = function() {
-        if(!name || typeof name !== "string") error("CreateMenu", "Menu should have name as first argument");
+/**
+ * @typedef { Function } Menu
+ * 
+ * @param { string } name Имя меню
+ */
+exports.createMenu = function(name, cb) {
+    const Validate = function() {
+        if(!name) return throw_error("CreateMenu", "Menu should have name as first argument");
     };
-
-    /** @private */
+    
     this.options = {
         name: name,
-        colors: {
-            background: [31, 31, 31, 255],
-            outline: [117, 117, 117, 125],
-            accent: [64, 64, 64, 255],
-            accent_color: [147, 94, 250, 255],
-            fields: [44, 46, 49, 255],
-            text: [241, 241, 241, 255],
-            shadow: [0, 0, 0, 255],
-    
-            elements: {
-                checkbox: {
-                    inner_background: [32, 32, 32, 255]
-                },
-    
-                button: {
-                    inner_background: [32, 32, 32, 255]
-                },
-    
-                slider: {
-                    inner_background: [32, 32, 32, 255],
-                    inner_line: [52, 52, 52, 255]
-                },
-    
-                dropdown: {
-                    inner_background: [32, 32, 32, 255]
-                },
-    
-                colorpicker: {
-                    inner_background: [32, 32, 32, 255]
-                }
+        build: build || "release",
+        sizes: {
+            menu: {
+                width: 456,
+                height: 340,
+            },
+
+            navigation: {
+                width: 65
+            },
+
+            fields: {
+                width: 180
             }
-        },
-        size: { width: 540, height: 440 }
+        }
     };
 
-    /** @private */
-    this.ui = {
-        pos_x: ["Script items", this.options.name + "_x"].join(", "),
-        pos_y: ["Script items", this.options.name + "_y"].join(", "),
-        scale: ["Script items", this.options.name + "_scale"].join(", "),
-        animSpeed: ["Script items", this.options.name + "_anim"].join(", "),
+    this.themes = {
+        dark: {
+            theme: [32, 32, 32, 255],
+            header: [31, 31, 31, 255],
+            inner: [22, 22, 22, 255],
+            stroke: [37, 37, 37, 255],
+            icons: [74, 74, 74, 255],
+            accent: [2, 121, 214, 255],
+
+            text: [205, 205, 205, 255],
+            shadow: [0, 0, 0, 255]
+        }
     };
+
+    this.data = {
+        tabs: {},
+        current_tab: "Rage",
+
+        menu: {},
+
+        checkboxes: [],
+        buttons: [],
+        colorpickers: [],
+        dropdowns: [],
+        multi_dropdowns: [],
+        textboxes: [],
+        hotkeys: [],
+
+        items: [],
+        active: []
+    };
+
+    this.animations = {
+        menu: {
+            box: {
+                width: 1,
+                height: 1,
+            },
+
+            subtabs: 1
+        }
+    }
 
     /** @private */
     this.Preload = function() {
         UI.AddSliderInt(this.options.name + "_x", 0, screen[0]);
         UI.AddSliderInt(this.options.name + "_y", 0, screen[1]);
-        UI.AddSliderFloat(this.options.name + "_scale", 0.80, 1.5);
-        UI.AddSliderFloat(this.options.name + "_anim", 0.5, 3);
 
-        if(build != "dev") {
+        if(build !== "dev") {
             UI.SetEnabled(this.options.name + "_x", 0);
             UI.SetEnabled(this.options.name + "_y", 0);
-            UI.SetEnabled(this.options.name + "_scale", 0);
-            UI.SetEnabled(this.options.name + "_anim", 0);
         };
     };
 
@@ -98,49 +97,107 @@ exports.CreateMenu = function(name, cb) {
         return this.options;
     };
 
-    this.GetUI = function() {
-        return this.ui;
-    };
-
-    this.GetScale = function() {
-        return 1;
-    };
-
-    this.GetPosition = function() {
-        return [UI.GetValue("Script items", this.options.name + "_x"), UI.GetValue("Script items", this.options.name + "_y")];
-    };
-
-    this.GetSize = function(scaleIncluded, animationIncluded) {
-        return [this.options.size.width, this.options.size.height];
-    };
-
     this.GetSource = function() {
         return this;
     };
 
-    this.DrawUI = function(textScaling) {
-        const position = this.GetPosition();
-        const size = this.GetSize(true, true);
-        const options = this.GetOptions();
-        const scale = this.GetScale();
-
-        const name = options.name;
-        const colors = options.colors;
-        const fonts = {
-            logo: Render.AddFont("Verdana.ttf", 18 * (textScaling ? scale : 1), 400),
-            tab: Render.AddFont("Verdana.ttf", 18 * (textScaling ? scale : 1), 400)
-        };
-
-        Render.FilledRect(position[0], position[1], size[0], size[1], colors.background); // Главное меню
-
-        Render.FilledRect(position[0] + 10, position[1] + Render.TextSizeCustom(name, fonts.logo)[1] + 25, (size[0] - 30) / 2, size[1] - (Render.TextSizeCustom(name, fonts.logo)[1] + 35), colors.fields); // Левый Field
-        Render.FilledRect(position[0] + 20 + (size[0] - 30) / 2, position[1] + Render.TextSizeCustom(name, fonts.logo)[1] + 25, (size[0] - 29) / 2, size[1] - (Render.TextSizeCustom(name, fonts.logo)[1] + 35), colors.fields); // Правый Field    
-
-        Render.StringCustom(position[0] + 11, position[1] + 11, 0, name, colors.shadow, fonts.logo); // Тень логотипа
-        Render.StringCustom(position[0] + 11, position[1] + 11, 0, name, colors.text, fonts.logo); // Сам логотип
+    this.GetThemes = function() {
+        return this.themes;
     };
-    
-    validation();
+
+    this.GetPosition = function() {
+        return [
+            UI.GetValue(this.options.name + "_x"), 
+            UI.GetValue(this.options.name + "_y")
+        ];
+    };
+
+    this.GetData = function() {
+        return this.data;
+    };
+
+    this.GetAnimations = function() {
+        return this.animations;
+    };
+
+    this.DrawTabs = function() {
+        const tab_keys = Object.keys(this.data.tabs);
+        const pos = this.GetPosition();
+        const colors = this.GetThemes();
+        const data = this.GetData();
+        const sizes = this.GetOptions().sizes;
+        
+        const font = Render.AddFont("Segoe UI", 16, 600);
+
+        data.menu["navigation"] = {};
+        data.menu["navigation"].add_height = 0;
+
+        tab_keys.forEach(function(tab) {
+            Render.StringCustom(pos[0] + 32, (pos[1] + 10) + data.menu["navigation"].add_height, 1, tab, data.current_tab === tab ? colors["dark"].accent : colors["dark"].text, font);
+
+            if(
+                Useful.Other.CursorBox(
+                    Input.GetCursorPosition(),
+                    pos[0] + 32, 
+                    (pos[1] + 10) + data.menu["navigation"].add_height, 
+                    pos[0] + 32 + Render.TextSizeCustom(tab, font)[0] + 5, 
+                    (pos[1] + 10) + data.menu["navigation"].add_height + Render.TextSizeCustom(tab, font)[1] + 5
+                ) && Input.IsKeyPressed(0x01)
+            ) data["current_tab"] = tab
+
+            data.menu["navigation"].add_height += Render.TextSizeCustom(tab, font)[1] + 10;
+
+            if(data.menu["navigation"].add_height > (sizes.menu.height - 10)) sizes.menu.height = data.menu["navigation"].add_height + 10
+        });
+    };
+
+    this.DrawAnimations = function() {
+        const animations = this.GetAnimations();
+
+        animations.menu["box"].height = Useful.Math.Lerp(animations.menu["box"].height, UI.IsMenuOpen() ? 1 : 0, 0.095)
+    };
+
+    this.DrawUI = function(DrawAnimations) {
+        const options = this.GetOptions();
+        const colors = this.GetThemes();
+        const pos = this.GetPosition();
+        const box_h_anim = (this.GetAnimations().menu["box"].height).toFixed(3);
+        const sizes = options["sizes"];
+
+        if(DrawAnimations) {
+            this.DrawAnimations();
+            if(box_h_anim <= 0.01) return;
+        }
+
+        Render.FilledRect(pos[0], pos[1], sizes["menu"].width, sizes["menu"].height * box_h_anim, colors["dark"].theme);
+
+        Render.FilledRect(pos[0], pos[1], sizes["navigation"].width, sizes["menu"].height * box_h_anim, colors["dark"].inner);
+        Render.Rect(pos[0], pos[1], sizes["navigation"].width, sizes["menu"].height * box_h_anim, colors["dark"].stroke);
+
+        Render.FilledRect((pos[0] + sizes["navigation"].width) + 10, (pos[1] + 10), sizes["fields"].width, (sizes["menu"].height - 20) * box_h_anim, colors["dark"].inner);
+        Render.Rect((pos[0] + sizes["navigation"].width) + 10, (pos[1] + 10), sizes["fields"].width, (sizes["menu"].height - 20) * box_h_anim, colors["dark"].stroke);
+
+        Render.FilledRect((pos[0] + sizes["navigation"].width + sizes["fields"].width) + 20, (pos[1] + 10), sizes["fields"].width, (sizes["menu"].height - 20) * box_h_anim, colors["dark"].inner);
+        Render.Rect((pos[0] + sizes["navigation"].width + sizes["fields"].width) + 20, (pos[1] + 10), sizes["fields"].width, (sizes["menu"].height - 20) * box_h_anim, colors["dark"].stroke);
+    };
+
+    this.AddSubtab = function(name) {
+        if(this.data.tabs[name]) return throw_error("AddSubtab", "You can't create 2 tabs with same name");
+
+        this.data.tabs[name] = {
+            General: {
+                elements: [],
+                add_height: 0
+            },
+
+            Other: {
+                elements: [],
+                add_height: 0
+            }
+        };
+    };
+
+    Validate();
     this.Preload();
     cb(this);
 
