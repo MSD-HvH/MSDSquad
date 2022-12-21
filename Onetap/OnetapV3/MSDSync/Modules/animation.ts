1d5b01dfc003eee2364e6a572f1c18983c8e930c
Mased <68379695+MasedMSD@users.noreply.github.com>
2022-12-21 22:07:56 +0500

    Create animation.ts

---
+++ Onetap/OnetapV3/MSDSync/Modules/animation.ts
@@ -0,0 +1,86 @@
+// @ts-ignore
+const Useful = require("./useful.ts");
+const AnimationItems = [];
+
+const Lerp = function(time, start, end_pos) {
+    if (time == undefined) {
+        time = 0.095
+    }
+
+    time = Useful.Math.Clamp(Globals.Frametime() * (time * 175), 0, 1)
+
+    if (typeof(start) == 'object') {
+        var start_color = start
+        var end_color = end_pos
+
+        start_color[0] = Lerp(time, start_color[0], end_color[0])
+        start_color[1] = Lerp(time, start_color[1], end_color[1])
+        start_color[2] = Lerp(time, start_color[2], end_color[2])
+        start_color[3] = Lerp(time, start_color[3], end_color[3])
+        return start_color
+    }
+    
+    var delta = end_pos - start
+    delta = delta * time
+    delta = delta + start
+
+    if (end_pos == 0 && delta < 0.01 && delta > -0.01) delta = 0
+    else if (end_pos == 1 && delta < 1.01 && delta > 0.99) delta = 1
+
+    return delta
+}
+
+const ColorEquals = function(firs_color, second_color) {
+    return firs_color[0] == second_color[0] && firs_color[1] == second_color[1] && firs_color[2] == second_color[2] && firs_color[3] == second_color[3] 
+}
+
+const UpdateAnimations = function() {
+    for (var k in AnimationItems) {
+        if (!AnimationItems[k] || !AnimationItems[k].called_this_frame) {
+            if ( typeof(GetAnimation(k).number) == 'object') {
+                if (ColorEquals(NewAnimation(k, [0, 0, 0, 0], true), [0, 0, 0, 0])) {
+                    AnimationItems[k] = undefined
+                }
+            }
+            else {
+                if (NewAnimation(k, 0, true) == 0) {
+                    AnimationItems[k] = undefined
+                }
+            }
+            continue
+        }
+        
+        AnimationItems[k].called_this_frame = false
+    };
+};
+
+const NewAnimation = function(name, new_value, removing) {
+    if (!AnimationItems[name]) {
+        AnimationItems[name] = {}
+        AnimationItems[name].color = [0, 0, 0, 0]
+        AnimationItems[name].number = 0
+        AnimationItems[name].called_this_frame = true
+    }
+
+    if (removing == undefined) {   
+        AnimationItems[name].called_this_frame = true
+    }
+
+    if (typeof(new_value) == 'object') {
+        var lerping = Lerp(0.095, AnimationItems[name].color, new_value)
+        AnimationItems[name].color = lerping
+
+        return lerping
+    }
+
+    var lerping = Lerp(0.095, AnimationItems[name].number, new_value)
+    AnimationItems[name].number = lerping
+
+    return lerping
+};
+
+const GetAnimation = function(name) {
+    return !AnimationItems[name] ? {number : 0, color : [0, 0, 0, 0], called_this_frame : false} : AnimationItems[name]
+};
+
+module.exports = { AnimationItems, ColorEquals, UpdateAnimations, NewAnimation, GetAnimation };
