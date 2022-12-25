5373e9186f775140eab2e8fd865e3cde793626ea
Mased <68379695+MasedMSD@users.noreply.github.com>
2022-12-25 18:08:22 +0500

    Update index.ts

--- Onetap/OnetapV3/MSDSync/index.ts
+++ Onetap/OnetapV3/MSDSync/index.ts
@@ -1,16 +1,3 @@
-// @ts-nocheck
+import { Menu } from "./MSDSync/Structures";
 
-const Useful = require("./Modules/useful.ts");
-const Timeouting = require("./Modules/timeout.ts");
-const Animation = require("./Modules/animation.ts");
-
-const information = [
-    ["\n> MSDSync Welcomes you!", [255, 255, 255, 255]],
-    ["Welcome, " + Cheat.GetUsername() + "!", [163, 191, 115, 255]],
-];
-
-information.forEach(function(stroke) {
-    Cheat.PrintColor(stroke[1], stroke[0] + "\n");
-});
-
-module.exports = { Useful, Timeouting, Animation };
+const menu = new Menu("MSDSync").CreateMenu();
