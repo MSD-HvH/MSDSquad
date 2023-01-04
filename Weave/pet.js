12b790ebad06c2736f92ac50e2a2d0db737532f4
Mased <68379695+MasedMSD@users.noreply.github.com>
2023-01-04 20:01:24 +0500

    Delete Weave directory

--- Weave/pet.js
+++
@@ -1,6 +0,0 @@
-register_callback("render", function() {
-    var pos = entity.get_origin(entity.get_local_player())
-    var w2s = render.world_to_screen(pos)
-
-    render.picture("C:/weave/pet.png", [w2s[0] - 100, w2s[1] - 50], [70, 70], 255);
-})
