80ed39d9f2cf202bf5eeb73721bcbbc8dd431ec2
Mased <68379695+MasedMSD@users.noreply.github.com>
2023-01-04 19:33:16 +0500

    Add files via upload

---
+++ Onetap/OnetapV3/MSDSync/Core/CoreJS/internals/array-with.ts
@@ -0,0 +1,15 @@
+import { lengthOfArrayLike } from "./length-of-array-like";
+import { toIntegerOrInfinity } from "./to-integer-or-infinity";
+
+// https://tc39.es/proposal-change-array-by-copy/#sec-array.prototype.with
+// https://tc39.es/proposal-change-array-by-copy/#sec-%typedarray%.prototype.with
+module.exports = function (O, C, index, value) {
+  const len = lengthOfArrayLike(O);
+  const  relativeIndex = toIntegerOrInfinity(index);
+  const  actualIndex = relativeIndex < 0 ? len + relativeIndex : relativeIndex;
+  if (actualIndex >= len || actualIndex < 0) throw RangeError('Incorrect index');
+  const  A = new C(len);
+  let  k = 0;
+  for (; k < len; k++) A[k] = k === actualIndex ? value : O[k];
+  return A;
+};
