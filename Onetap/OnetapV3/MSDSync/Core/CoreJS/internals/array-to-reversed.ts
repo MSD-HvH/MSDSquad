ad559bc56db55d761c1b4773707f3ec283b21e8c
Mased <68379695+MasedMSD@users.noreply.github.com>
2023-01-04 19:29:02 +0500

    Update array-to-reversed.ts

--- Onetap/OnetapV3/MSDSync/Core/CoreJS/internals/array-to-reversed.ts
+++ Onetap/OnetapV3/MSDSync/Core/CoreJS/internals/array-to-reversed.ts
@@ -3,9 +3,9 @@ import { lengthOfArrayLike } from "./length-of-array-like";
 // https://tc39.es/proposal-change-array-by-copy/#sec-array.prototype.toReversed
 // https://tc39.es/proposal-change-array-by-copy/#sec-%typedarray%.prototype.toReversed
 export const arrayToReversed = (O, C) => {
-  var len = lengthOfArrayLike(O);
-  var A = new C(len);
-  var k = 0;
+  const len = lengthOfArrayLike(O);
+  const A = new C(len);
+  let k = 0;
   for (; k < len; k++) A[k] = O[len - k - 1];
   return A;
 };
