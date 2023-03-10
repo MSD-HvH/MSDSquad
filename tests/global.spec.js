a108a687df18a975d073b4e628645914226a74b9
MasedMSD <roblox.hack@mail.ru>
2023-03-10 17:12:27 +0500

    performance

--- tests/global.spec.js
+++ tests/global.spec.js
@@ -3,6 +3,7 @@ const global = new Function("return this")();
 Cheat.Print("\n---------------- Global Tests ----------------\n");
 
 Cheat.Print("\nglobal objects: \n - " + Object.keys(global).join("\n - ") + "\n\n");
-Cheat.Print("__filename is: " + __filename + "\n\n");
+Cheat.Print("__filename is: " + __filename + "\n");
+Cheat.Print("performance.now() is: " + performance.now() + "\n\n" )
 
 Cheat.Print("\n---------------- Global Tests End ----------------\n");
