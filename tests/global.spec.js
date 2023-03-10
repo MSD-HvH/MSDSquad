46eb51676b10bfc33223feea643db2c0761a27e4
MasedMSD <roblox.hack@mail.ru>
2023-03-10 17:13:00 +0500

    removed space

--- tests/global.spec.js
+++ tests/global.spec.js
@@ -4,6 +4,6 @@ Cheat.Print("\n---------------- Global Tests ----------------\n");
 
 Cheat.Print("\nglobal objects: \n - " + Object.keys(global).join("\n - ") + "\n\n");
 Cheat.Print("__filename is: " + __filename + "\n");
-Cheat.Print("performance.now() is: " + performance.now() + "\n\n" )
+Cheat.Print("performance.now() is: " + performance.now() + "\n\n");
 
 Cheat.Print("\n---------------- Global Tests End ----------------\n");
