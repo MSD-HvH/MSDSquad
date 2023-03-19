58e40648111e9dfa56da09378589c34b2ce05c81
MasedMSD <roblox.hack@mail.ru>
2023-03-19 20:21:58 +0500

    some fixes

---
+++ MSDSync.ts
@@ -0,0 +1,4 @@
+const build: "Dev" | "Release" = (["Mased"].join(" ")).match(Cheat.GetUsername()) ? "Dev" : "Release";
+const screen = Render.GetScreenSize();
+const version: string = "1.0.0";
+
---
+++ MSDSync/Games/Minesweeper.ts
@@ -0,0 +1,8 @@
+/**
+ * Minesweeper
+ * 
+ * @author Masd
+ * @version 1.0.0
+ * 
+ * Discord: Mased#1854
+ */
