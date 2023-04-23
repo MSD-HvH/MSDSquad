30af28a1f623a40cea472fac07ae2d352452be83
MasedMSD <roblox.hack@mail.ru>
2023-04-23 12:36:36 +0500

    space removes

--- tests/gamesense.spec.js
+++ tests/gamesense.spec.js
@@ -1,5 +1,4 @@
 const GamesenseStyle = require("../MSDSync/Elements/SolusUI/index.js").GamesenseStyle;
-
 const test = new GamesenseStyle({ x: 100, y: 100, width: 160, height: 25 });
 
 const on_Draw = function () {
--- tests/solusv2.spec.js
+++ tests/solusv2.spec.js
@@ -1,5 +1,4 @@
 const SolusV2 = require("../MSDSync/Elements/SolusUI/index.js").SolusV2;
-
 const test = new SolusV2({ x: 100, y: 100, width: 160, height: 25 });
 
 const on_Draw = function () {
